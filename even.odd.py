7fa6c3a247af0b5f4d1fb4c1742bae5b716875f2
sweetyram <117337661+sweetyram@users.noreply.github.com>
2022-11-20 20:29:07 +0530

    no of even and odd question 3

---
+++ even.odd.py
@@ -0,0 +1,13 @@
+t = (1,2,3,4,5,6,7,8,9)
+print("even and odd items = ", t)
+teven = todd = 0
+for i in range(len(t)) :
+  if (t[i] % 2) == 0 :
+    teven = teven+1
+  else :
+    todd = todd+1
+print("the num of even numbers in t = ",teven)
+print("the num of odd numbers in t = ", todd)
+ 
+
+       
